8573fb1e4c5694ab7769816cd8342b7302143012
euxzy <rijal.m12l@gmail.com>
2023-01-06 15:50:13 +0700

    [ + ] initialize modal add admin

---
+++ resources/views/livewire/components/modal/add-admin.blade.php
@@ -0,0 +1,11 @@
+<div class="absolute w-[575px]">
+  <div class="relative bg-white pt-8">
+    <div class="relative mx-auto mb-12 flex w-max flex-col items-center justify-center gap-3">
+      <h1 class="text-3xl font-bold text-navy">Form <span class="text-orange-2">Create</span> Admin</h1>
+      <div
+        class="relative h-2 w-[340px] rounded-lg bg-orange-1 after:absolute after:inset-0 after:m-auto after:h-5 after:w-16 after:rounded-xl after:bg-navy">
+      </div>
+    </div>
+  </div>
+  <div class="absolute inset-0 bg-gray-1/40"></div>
+</div>
--- resources/views/livewire/dashboard/list-admins.blade.php
+++ resources/views/livewire/dashboard/list-admins.blade.php
@@ -16,7 +16,7 @@
   @livewire('partials.sidebar')
 
   <section class="mt-20 w-full py-10 lg:mt-0 lg:w-[80%]">
-    <div class="mx-auto w-11/12 rounded-xl pb-6 lg:mx-0 lg:w-full lg:bg-gray-1 lg:p-6">
+    <div class="mx-auto w-11/12 rounded-xl pb-6 lg:mx-0 lg:w-full lg:bg-gray-1 lg:p-6" x-data="{ isAddActive: false }">
       <div class="relative mx-auto mb-12 flex w-max flex-col items-center justify-center gap-3">
         <h1 class="text-3xl font-bold text-orange-2">List <span class="text-navy">Admin</span></h1>
         <div
@@ -25,10 +25,12 @@
       </div>
 
       <div class="mb-6">
-        <a href="#"
-          class="inline-block rounded-md bg-white px-6 py-2 font-semibold text-navy shadow-md shadow-navy/30 transition-all duration-300 hover:-translate-y-0.5 hover:shadow-lg hover:shadow-navy/40">Create
+        <a x-on:click="isAddActive = true"
+          class="inline-block cursor-pointer rounded-md bg-white px-6 py-2 font-semibold text-navy shadow-md shadow-navy/30 transition-all duration-300 hover:-translate-y-0.5 hover:shadow-lg hover:shadow-navy/40">Create
           Admin</a>
       </div>
+
+      {{-- @livewire('components.modal.add-admin') --}}
 
       <div class="bg-white" x-data="listAdmin" x-init="getAdmins()">
         <ul class="flex gap-3 bg-orange-1 px-3 py-4 font-semibold text-navy">
