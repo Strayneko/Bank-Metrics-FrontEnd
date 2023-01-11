4d4eb1ca0bd8e94f9aed98b2a4f4e6f9f4c6b0a5
euxzy <rijal.m12l@gmail.com>
2023-01-11 15:01:47 +0700

    [ + ] add list submission on user dashboard

--- resources/views/livewire/user/home.blade.php
+++ resources/views/livewire/user/home.blade.php
@@ -1,25 +1,38 @@
 <section class="mt-20 w-full py-10 text-navy lg:mt-0 lg:w-[80%]">
-    <div class="mx-auto w-11/12 rounded-xl pb-6 lg:mx-0 lg:w-full lg:bg-gray-1 lg:p-6">
-        <h1 class="mx-auto mb-10 w-max text-3xl font-bold" x-text="'Welcome ' + resData.data.name"></h1>
+  <div class="mx-auto w-11/12 rounded-xl pb-6 lg:mx-0 lg:w-full lg:bg-gray-1 lg:p-6">
+    <h1 class="mx-auto mb-10 w-max text-3xl font-bold" x-text="'Welcome ' + resData.data.name"></h1>
 
-        <template x-if="resData.data.profile">
-            <div class="flex justify-center gap-10 bg-white">
-                <div class="bg-orange-1">
-                    <div>
-                        <img class="w-full" src="" alt="ini gambar">
-                    </div>
-                </div>
-                <div>
-                    <p x-text="resData.data."></p>
-                    <p>Hello</p>
-                </div>
-            </div>
-        </template>
+    <template x-if="resData.data.profile">
+      <div class="overflow-hidden rounded-xl bg-white">
+        <ul class="flex gap-3 bg-orange-1 px-3 py-4 font-semibold text-navy">
+          <li class="w-10 text-center">No</li>
+          <li class="w-64">Date</li>
+          <div class="hidden gap-3 lg:flex">
+            <li class="w-56">Bank</li>
+            <li class="w-80">Status</li>
+            <li>Action</li>
+          </div>
+        </ul>
 
-        <template x-if="!resData.data.profile">
-            <div class="flex min-h-[100px] items-center justify-center rounded-xl bg-white">
-                <p class="text-xl font-semibold">Please fill in your personal data first!</p>
+        <div class="bg-white">
+          <ul class="flex gap-3 px-3 py-4 font-semibold text-navy">
+            <li class="w-10 text-center">1</li>
+            <li class="w-64">2023-01-01</li>
+            <div class="hidden gap-3 lg:flex">
+              <li class="w-56">Bank Ini</li>
+              <li class="w-80">Accepted</li>
+              <li><a href="#">Detail</a></li>
             </div>
-        </template>
-    </div>
+          </ul>
+        </div>
+
+      </div>
+    </template>
+
+    <template x-if="!resData.data.profile">
+      <div class="flex min-h-[100px] items-center justify-center rounded-xl bg-white">
+        <p class="text-xl font-semibold">Please fill in your personal data first!</p>
+      </div>
+    </template>
+  </div>
 </section>
