57d8f2dd2b1ea8cab8b13f4d0136e7bf31b1483c
euxzy <rijal.m12l@gmail.com>
2023-01-11 13:20:24 +0700

    [ + ] fetch country and create responsive user profile

--- resources/views/livewire/components/modal/user-modal.blade.php
+++ resources/views/livewire/components/modal/user-modal.blade.php
@@ -1,20 +1,20 @@
 <div class="relative w-full">
-  <div class="absolute inset-x-0 -top-40 z-20 mx-auto mb-48 w-[575px]">
+  <div class="absolute inset-x-0 -top-40 z-20 mx-auto mb-48 lg:w-[575px]">
     <div class="relative mb-10 rounded-xl bg-white pb-10 pt-8 shadow-lg shadow-navy/50">
       <div class="relative mx-auto mb-12 flex w-max flex-col items-center justify-center gap-3">
-        <h1 class="text-3xl font-bold text-navy">Update <span class="text-orange-2">User</span> Profile</h1>
+        <h1 class="text-2xl font-bold text-navy lg:text-3xl">Update <span class="text-orange-2">User</span> Profile</h1>
         <div
-          class="relative h-2 w-[340px] rounded-lg bg-orange-1 after:absolute after:inset-0 after:m-auto after:h-5 after:w-16 after:rounded-xl after:bg-navy">
+          class="relative h-2 w-full rounded-lg bg-orange-1 after:absolute after:inset-0 after:m-auto after:h-5 after:w-16 after:rounded-xl after:bg-navy lg:w-[340px]">
         </div>
       </div>
-      <form class="flex cursor-pointer flex-col px-[39px]" enctype="multipart/form-data"
-        x-on:submit.prevent="updateProfile()" x-ref="userForm">
+      <form class="flex cursor-pointer flex-col px-10" enctype="multipart/form-data" x-on:submit.prevent="updateProfile()"
+        x-ref="userForm">
         <label for="name" class="relative w-full pb-3">
           <p class="font-bold text-navy">Name</p>
           <input type="text" name="name" id="name" class="peer mt-2 outline-none"
             placeholder="Insert your name" :value="userData.name" disabled>
           <div
-            class="absolute inset-x-0 bottom-0 mx-auto block h-1 w-0 rounded-lg bg-navy transition-all duration-500 peer-hover:w-full">
+            class="absolute inset-x-0 bottom-0 mx-auto block h-0.5 w-0 rounded-lg bg-navy transition-all duration-300 peer-hover:w-full">
           </div>
         </label>
 
@@ -23,25 +23,25 @@
           <input type="email" name="email" id="email" class="peer mt-2 outline-none"
             placeholder="Insert your email" :value="userData.email" disabled>
           <div
-            class="absolute inset-x-0 bottom-0 mx-auto block h-1 w-0 rounded-lg bg-navy transition-all duration-500 peer-hover:w-full">
+            class="absolute inset-x-0 bottom-0 mx-auto block h-0.5 w-0 rounded-lg bg-navy transition-all duration-300 peer-hover:w-full">
           </div>
         </label>
 
-        <label for="address" class="relative w-full pb-3">
+        <label for="address" class="relative w-full pb-0.5">
           <p class="font-bold text-navy">Address</p>
-          <textarea type="text" name="address" id="address" class="peer mt-2 w-full outline-none"
-            placeholder="Insert your min age"></textarea>
+          <textarea type="text" name="address" id="address" class="peer mt-2 min-h-[50px] w-full outline-none"
+            placeholder="Insert your address" required></textarea>
           <div
-            class="absolute inset-x-0 bottom-0 mx-auto block h-1 w-0 rounded-lg bg-navy transition-all duration-500 peer-hover:w-full">
+            class="absolute inset-x-0 bottom-0 mx-auto block h-0.5 w-0 rounded-lg bg-navy transition-all duration-300 peer-hover:w-full">
           </div>
         </label>
 
         <label for="dob" class="relative w-full pb-3">
           <p class="font-bold text-navy">Date of Birth</p>
-          <input type="date" name="dob" id="dob" class="peer mt-2 w-full outline-none"
-            placeholder="Insert your date of birth" x-model="dob">
+          <input type="date" name="dob" id="dob" class="peer mt-2 w-full text-navy outline-none"
+            placeholder="Insert your date of birth" x-model="dob" required>
           <div
-            class="absolute inset-x-0 bottom-0 mx-auto block h-1 w-0 rounded-lg bg-navy transition-all duration-500 peer-hover:w-full">
+            class="absolute inset-x-0 bottom-0 mx-auto block h-0.5 w-0 rounded-lg bg-navy transition-all duration-300 peer-hover:w-full">
           </div>
         </label>
 
@@ -61,46 +61,49 @@
 
         <label for="photo" class="relative w-full pb-3">
           <p class="font-bold text-navy">Photo</p>
-          <input type="file" name="photo" id="photo"
-            class="peer mt-2 w-full rounded-md bg-orange-1 p-2 outline-orange-1">
+          <input type="file" name="photo" id="photo" required
+            class="peer relative z-10 w-full cursor-pointer rounded-md border-2 border-orange-1 bg-white font-semibold text-gray-3 transition-all duration-150 file:mr-3 file:cursor-pointer file:border-none file:bg-orange-1 file:px-4 file:py-2 file:font-medium file:text-navy file:outline-none before:absolute before:left-0 before:top-0 before:-z-10 before:block before:h-full before:w-0 before:bg-orange-1 before:transition-all before:duration-300 hover:text-navy/60 hover:before:w-full">
         </label>
 
         <label for="country_id" class="relative w-full pb-3">
           <p class="font-bold text-navy">Nationality</p>
-          <select name="country_id" id="country_id" class="w-full rounded-md border border-orange-2 bg-orange-1 p-2">
-            <option selected disabled class="font-semibold text-white">Choose your nationality</option>
-            <option value="0" class="font-medium text-white">Indonesia</option>
-            <option value="1" class="font-medium text-white">America</option>
-            <option value="2" class="font-medium text-white">Other</option>
+          <select name="country_id" id="country_id" required
+            class="w-full cursor-pointer rounded-md border-2 border-orange-1 bg-white p-2 font-medium text-navy outline-none transition-all duration-300 hover:bg-orange-1">
+            <option selected disabled class="bg-white font-semibold">Choose your nationality</option>
+            <template x-for="country of dataCountry">
+              <option :value="country.id" class="bg-white font-medium" x-text="country.name"></option>
+            </template>
           </select>
         </label>
 
         <label for="marital_status" class="relative w-full pb-3">
           <p class="font-bold text-navy">Marital Status</p>
           <select name="marital_status" id="marital_status"
-            class="w-full rounded-md border border-orange-2 bg-orange-1 p-2">
-            <option selected disabled class="font-semibold text-white">Choose your marital status</option>
-            <option value="0" class="font-medium text-white">Single</option>
-            <option value="1" class="font-medium text-white">Married</option>
+            class="w-full cursor-pointer rounded-md border-2 border-orange-1 bg-white p-2 font-medium text-navy outline-none transition-all duration-300 hover:bg-orange-1"
+            required>
+            <option selected disabled class="bg-white font-semibold">Choose your marital status</option>
+            <option value="0" class="bg-white font-medium">Single</option>
+            <option value="1" class="bg-white font-medium">Married</option>
           </select>
         </label>
 
         <label for="employement" class="relative w-full pb-3">
           <p class="font-bold text-navy">Employement</p>
           <select name="employement" id="employement"
-            class="w-full rounded-md border border-orange-2 bg-orange-1 p-2">
-            <option selected disabled class="font-semibold text-white">Choose your employement</option>
-            <option value="0" class="font-medium text-white">Full-Time</option>
-            <option value="1" class="font-medium text-white">Half-Time</option>
+            class="w-full cursor-pointer rounded-md border-2 border-orange-1 bg-white p-2 font-medium text-navy outline-none transition-all duration-300 hover:bg-orange-1"
+            required>
+            <option selected disabled class="bg-white font-semibold">Choose your employement</option>
+            <option value="0" class="bg-white font-medium">Full-Time</option>
+            <option value="1" class="bg-white font-medium">Half-Time</option>
           </select>
         </label>
 
-        <div class="mt-5 flex justify-between pb-12">
+        <div class="mt-5 flex flex-col justify-between gap-3 pb-4 lg:flex-row lg:gap-0">
           <button type="submit"
-            class="relative z-10 w-full rounded-lg border-2 border-orange-1 bg-transparent px-20 py-3 font-bold text-navy outline-none transition-all duration-300 before:absolute before:left-0 before:top-0 before:-z-10 before:block before:h-full before:w-0 before:bg-orange-1 before:transition-all before:duration-300 hover:text-white hover:before:w-full lg:w-max">Update
+            class="relative z-10 inline-block w-full rounded-lg border-2 border-orange-1 bg-transparent px-10 py-3 font-bold text-navy outline-none transition-all duration-300 before:absolute before:left-0 before:top-0 before:-z-10 before:block before:h-full before:w-0 before:bg-orange-1 before:transition-all before:duration-300 hover:text-white hover:before:w-full lg:w-max">Update
             Profile</button>
           <a x-on:click="isUpdate = false"
-            class="relative z-10 w-full rounded-lg border-2 border-orange-1 bg-transparent px-20 py-3 font-bold text-navy outline-none transition-all duration-300 before:absolute before:left-0 before:top-0 before:-z-10 before:block before:h-full before:w-0 before:bg-orange-1 before:transition-all before:duration-300 hover:text-white hover:before:w-full lg:w-max">Back</a>
+            class="relative z-10 inline-block w-full rounded-lg border-2 border-orange-1 bg-transparent py-3 px-20 text-center font-bold text-navy outline-none transition-all duration-300 before:absolute before:left-0 before:top-0 before:-z-10 before:block before:h-full before:w-0 before:bg-orange-1 before:transition-all before:duration-300 hover:text-white hover:before:w-full lg:w-max">Back</a>
         </div>
       </form>
     </div>
--- resources/views/livewire/user/profile.blade.php
+++ resources/views/livewire/user/profile.blade.php
@@ -23,11 +23,22 @@
         this.resData = await res.json()
         this.roleId = this.resData.data.role_id
         this.userData = this.resData.data
-        console.log(this.userData)
+        // console.log(this.userData)
 
         if (this.roleId != 1) {
           window.location.replace(`{{ route('home') }}`)
         }
+      })
+    },
+
+    dataCountry: [],
+    getCountry() {
+      fetch(`{{ env('API_URL') }}/api/countries`, {
+        method: 'GET'
+      }).then(async res => {
+        const data = await res.json()
+        console.log(data)
+        this.dataCountry = data.data
       })
     },
 
@@ -58,24 +69,27 @@ getProfile()">
   <section class="mt-20 w-full py-10 lg:mt-0 lg:w-[80%]">
     <div class="relative mx-auto w-11/12 rounded-xl pb-6 lg:mx-0 lg:w-full lg:bg-gray-1 lg:p-6 lg:pt-12">
       <div class="w-full" x-data="{ isUpdate: false }">
-        <div class="relative inset-x-0 mx-auto mb-10 w-[575px]">
+        <div class="relative inset-x-0 mx-auto mb-10 w-full lg:w-[575px]">
           <div class="relative rounded-xl bg-white py-8">
-            <div class="relative mx-auto mb-20 flex w-max flex-col items-center justify-center gap-3">
+            <div class="relative mx-auto mb-8 flex w-max flex-col items-center justify-center gap-3">
               <h1 class="text-3xl font-bold text-navy">User <span class="text-orange-2">Profile</span></h1>
               <div
                 class="relative h-2 w-[190px] rounded-lg bg-orange-1 after:absolute after:inset-0 after:m-auto after:h-5 after:w-16 after:rounded-xl after:bg-navy">
               </div>
             </div>
-            <div class="w-full" x-show="isUpdate" x-transition.duration.500ms>
+            <div class="w-full" x-show="isUpdate" x-transition.duration.500ms x-init="getCountry()">
               @livewire('components.modal.user-modal')
             </div>
             <div class="mb-3 flex flex-col items-center justify-center text-gray-2">
-              <div class="mb-3 h-24 w-24 overflow-hidden rounded-full">
-                <img class="w-full" src="{{ asset('assets/image1.jpeg') }}" alt="profile" />
+              <div
+                class="mb-3 flex h-32 w-32 items-center justify-center overflow-hidden rounded-full bg-gray-1 lg:h-48 lg:w-48">
+                <img class="w-full"
+                  :src="userData.profile ? userData.profile.photo : `{{ asset('assets/profile.svg') }}`"
+                  :alt="userData.name" />
               </div>
             </div>
-            <div class="mx-10 rounded-xl bg-orange-1 font-medium text-navy">
-              <ul class="p-10">
+            <div class="rounded-xl bg-orange-1 font-medium text-navy lg:mx-10">
+              <ul class="flex flex-col justify-center gap-1 p-6 lg:p-10">
                 <li class="flex gap-10">
                   <span class="w-48">Name</span>
                   <span class="w-full" x-text="userData.name"></span>
@@ -86,47 +100,45 @@ getProfile()">
                 </li>
 
                 <template x-if="!userData.profile">
-                  <li class="mt-6 text-center">ajsfagsfjhsafj</li>
+                  <li class="mt-6 text-center">Please Update Your Profile!</li>
                 </template>
 
                 <template x-if="userData.profile">
-                  <div>
+                  <div class="flex flex-col gap-1">
                     <li class="flex gap-10">
                       <span class="w-48">Address</span>
-                      <h1 class="w-full">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, totam.
-                      </h1>
+                      <span class="w-full" x-text="userData.profile.address"></span>
                     </li>
                     <li class="flex gap-10">
                       <span class="w-48">Date of Birth</span>
-                      <h1 class="w-full">2002-05-22</h1>
+                      <span class="w-full" x-text="userData.profile.dob"></span>
                     </li>
                     <li class="flex gap-10">
                       <span class="w-48">Gender</span>
-                      <h1 class="w-full">Female</h1>
+                      <span class="w-full" x-text="userData.profile.gender == 1 ? 'Male' : 'Female'"></span>
                     </li>
                     <li class="flex gap-10">
                       <span class="w-48">Nationality</span>
-                      <h1 class="w-full">America</h1>
+                      <span class="w-full" x-text="userData.profile.country.country_name"></span>
                     </li>
                     <li class="flex gap-10">
                       <span class="w-48">Marital Status</span>
-                      <h1 class="w-full">Married Only</h1>
+                      <span class="w-full" x-text="userData.profile.marital_status == 1 ? 'Married' : 'Single'"></span>
                     </li>
                     <li class="flex gap-10">
                       <span class="w-48">Employment</span>
-                      <h1 class="w-full">Full-Time</h1>
+                      <span class="w-full"
+                        x-text="userData.profile.employement == 1 ? 'Full-Time' : 'Half-Time'"></span>
                     </li>
                   </div>
                 </template>
 
               </ul>
             </div>
-            <div class="mx-10 mt-5 flex justify-between pb-12">
+            <div class="mt-5 flex justify-between pb-4 lg:mx-10">
               <button x-on:click="isUpdate = !isUpdate" type="submit"
                 class="relative z-10 w-full rounded-lg border-2 border-orange-1 bg-transparent px-20 py-3 font-bold text-navy outline-none transition-all duration-300 before:absolute before:left-0 before:top-0 before:-z-10 before:block before:h-full before:w-0 before:bg-orange-1 before:transition-all before:duration-300 hover:text-white hover:before:w-full lg:w-max">Update
                 Profile</button>
-              {{-- <a x-on:click="isAddActive = false"
-                class="relative z-10 w-full rounded-lg border-2 border-orange-1 bg-transparent px-20 py-3 font-bold text-navy outline-none transition-all duration-300 before:absolute before:left-0 before:top-0 before:-z-10 before:block before:h-full before:w-0 before:bg-orange-1 before:transition-all before:duration-300 hover:text-white hover:before:w-full lg:w-max">Back</a> --}}
             </div>
           </div>
         </div>
