7606aadc37af5cd731abb0b6a3440737560efc50
euxzy <rijal.m12l@gmail.com>
2023-01-12 15:49:12 +0700

    [ - ] delete unneeded files

--- resources/views/Template/head.blade.php
+++
@@ -1,7 +0,0 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="https://kit.fontawesome.com/3d647cac5e.js" crossorigin="anonymous"></script>
-</head>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Livewire\Dashboard\ListBank;
 // User
 use App\Http\Livewire\User\Profile;
 use App\Http\Livewire\User\UserSubmission;
-use App\Http\Livewire\Dashboard\Submission;
+// use App\Http\Livewire\Dashboard\Submission;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,12 +27,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeDashboard::class)->name('home');
 
 Route::get('/login', Login::class)->name('login');
-
 Route::get('/register', Register::class);
 
 Route::get('/dashboard/listadmin', ListAdmins::class)->name('admin.list');
 Route::get('/dashboard/listuser', ListUsers::class)->name('user.list');
-Route::get('/dashboard/submission', Submission::class)->name('submission.list');
+// Route::get('/dashboard/submission', Submission::class)->name('submission.list');
 Route::get('/dashboard/bank', ListBank::class)->name('bank.list');
 
 
