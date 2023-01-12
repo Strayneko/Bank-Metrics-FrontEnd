a1bd40b66293401d3b1bb76ad4609402cde15f43
Ciman Code <firmanabdul278@gmail.com>
2023-01-12 09:19:16 +0700

    (+) fix UI modal in detail submission

--- resources/views/livewire/components/modal/detail-submission.blade.php
+++ resources/views/livewire/components/modal/detail-submission.blade.php
@@ -1,6 +1,6 @@
 {{-- Start Seaction Detail Bang --}}
 <section class="absolute inset-x-0 mx-auto w-full pb-12">
-    <div class="relative z-20 mx-auto w-3/5 rounded-xl bg-white p-10 shadow-md shadow-navy/60">
+    <div class="relative z-20 mx-auto mr-40 mt-40 w-3/5 rounded-xl bg-white p-10 shadow-md shadow-navy/60">
         <div class="relative mx-auto mb-6 flex w-max flex-col items-center justify-center gap-3">
             <h1 class="text-3xl font-bold text-orange-2">Detail <span class="text-navy">Submission</span></h1>
             <div class="relative h-2 w-72 rounded-lg bg-orange-1 after:absolute after:inset-0 after:m-auto after:h-5 after:w-16 after:rounded-xl after:bg-navy">
@@ -24,9 +24,8 @@
                 <span class="w-full flex-1 text-[#028647]">Approved</span>
             </li>
         </ul>
-        <a x-on:click="isShow = false" class="rounded-lg bg-[#f1f0f0] px-20 py-3 font-bold text-navy outline outline-orange-1 hover:bg-orange-1 hover:text-white">Back</a>
+        <a x-on:click="showDetail = false" class="rounded-lg bg-[#f1f0f0] px-20 py-3 font-bold text-navy outline outline-orange-1 hover:bg-orange-1 hover:text-white">Back</a>
     </div>
-
-    <div class="fixed inset-0 z-10 bg-gray-1/40 backdrop-blur-sm" x-on:click="isShow = false"></div>
+    <div class="fixed inset-0 z-10 bg-gray-1/40 backdrop-blur-sm" x-on:click="showDetail = false"></div>
 </section>
 {{-- End Seaction Detail Bang --}}
