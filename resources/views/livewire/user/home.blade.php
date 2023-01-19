bb28bcac973c511deb608aedcac53a178a08ccae
euxzy <rijal.m12l@gmail.com>
2023-01-19 10:52:24 +0700

    [ ~ ] update pagination on dashboard user

--- resources/views/livewire/user/home.blade.php
+++ resources/views/livewire/user/home.blade.php
@@ -10,15 +10,12 @@
       loansData: [],
       startAt: 0,
       pages: [],
-      getSubmission(index) {
+      getSubmission() {
         this.showMessage = 'Please wait...'
 
         const reqTime = Date.now()
         const path = '/api/loan/list'
         const apiKey = generateKey(path, reqTime)
-        this.loansData = []
-
-        this.pageNumber = index
 
         fetch(`{{ env('API_URL') }}${path}`, {
           method: 'GET',
@@ -32,13 +29,9 @@
           // console.log(this.submissionData)
 
           // console.log(this.pageNumber)
-
           const start = this.pageNumber * this.size
           const end = start + this.size
-          // console.log(start)
-          // console.log(end)
-
-          this.startAt = start
+          // console.log(start, end)
 
           this.total = this.submissionData.data.loans.length
           // console.log(this.total)
@@ -63,6 +56,23 @@
           })
         })
       },
+      async viewPage(index) {
+        // console.log(this.submissionData)
+        this.loansData = []
+        this.pageNumber = index
+
+        const start = this.pageNumber * this.size
+        const end = start + this.size
+        // console.log(start, end)
+
+        this.startAt = start
+        this.loansData = await this.submissionData.data.loans.slice(start, end)
+        // console.log(this.loansData)
+
+        if (this.loansData.length == 0) {
+          this.showMessage = 'No data Submissions found!'
+        }
+      }
     }))
 
     Alpine.data('rejected', () => ({
@@ -105,7 +115,7 @@
     <h1 class="mx-auto mb-10 w-max text-3xl font-bold" x-text="'Welcome ' + resData.data.name"></h1>
 
     <template x-if="resData.data.profile">
-      <div class="relative rounded-xl bg-gray-1/30 lg:bg-white" x-data="submission" x-init="getSubmission(0)">
+      <div class="relative rounded-xl bg-gray-1/30 lg:bg-white" x-data="submission" x-init="getSubmission()">
         <ul class="flex gap-3 rounded-t-xl bg-orange-1 px-3 py-4 font-semibold text-navy">
           <li class="w-10 text-center">No</li>
           <li class="w-64">Date</li>
@@ -119,7 +129,6 @@
         <template x-if="loansData.length == 0">
           <div class="my-10 pb-10 text-center text-2xl font-bold text-navy">
             <h1 x-text="showMessage"></h1>
-
           </div>
 
         </template>
@@ -148,7 +157,7 @@
         <div class="flex justify-end py-8 px-10">
           <ul class="flex w-max items-center rounded-md font-semibold text-orange-1 outline outline-2 outline-orange-1">
             <li>
-              <button class="group py-2 pl-4 disabled:cursor-not-allowed" type="button" x-on:click="getSubmission(0)"
+              <button class="group py-1 pl-3 disabled:cursor-not-allowed" type="button" x-on:click="viewPage(0)"
                 :disabled="pageNumber == 0">
                 <svg width="22" height="22" viewBox="0 0 22 22" fill="none"
                   xmlns="http://www.w3.org/2000/svg">
@@ -161,8 +170,8 @@
             </li>
 
             <li>
-              <button class="group px-4 py-2 disabled:cursor-not-allowed" type="button"
-                x-on:click="getSubmission(pageNumber - 1)" :disabled="pageNumber == 0">
+              <button class="group py-1 pr-4 disabled:cursor-not-allowed" type="button"
+                x-on:click="viewPage(pageNumber - 1)" :disabled="pageNumber == 0">
                 <svg width="22" height="22" viewBox="0 0 22 22" fill="none"
                   xmlns="http://www.w3.org/2000/svg">
                   <path d="M20 4L12 11L20 19" stroke="#FCC997" stroke-width="3.77953" stroke-linejoin="round"
@@ -171,17 +180,29 @@
               </button>
             </li>
 
+            <li class="-mr-[.25px]">
+              <button type="button" x-on:click="viewPage(0)"
+                class="flex h-9 w-6 cursor-pointer items-center justify-center outline outline-1 outline-orange-1"
+                :class="pageNumber == 0 ? 'bg-orange-1 text-white' : ''">1</button>
+            </li>
+
             <template x-for="num in pages">
-              <li class="-mr-[.25px]">
-                <button type="button" x-on:click="getSubmission(num)"
-                  class="flex h-10 w-8 cursor-pointer items-center justify-center outline outline-1 outline-orange-1"
+              <li class="-mr-[.25px]" :hidden="pageNumber != num || pageNumber == 0 || pageNumber == pages.length - 1">
+                <button type="button" x-on:click="viewPage(num)"
+                  class="flex h-9 w-6 cursor-pointer items-center justify-center outline outline-1 outline-orange-1"
                   x-text="num + 1" :class="pageNumber == num ? 'bg-orange-1 text-white' : ''"></button>
               </li>
             </template>
 
+            <li class="-mr-[.25px]">
+              <button type="button" x-on:click="viewPage(pages.length - 1)"
+                class="flex h-9 w-6 cursor-pointer items-center justify-center outline outline-1 outline-orange-1"
+                x-text="pages.length" :class="pageNumber == pages.length - 1 ? 'bg-orange-1 text-white' : ''"></button>
+            </li>
+
             <li>
-              <button class="group px-4 py-2 disabled:cursor-not-allowed" type="button"
-                x-on:click="getSubmission(pageNumber + 1)" :disabled="pageNumber == (pages.length - 1)">
+              <button class="group py-1 pl-4 disabled:cursor-not-allowed" type="button"
+                x-on:click="viewPage(pageNumber + 1)" :disabled="pageNumber == (pages.length - 1)">
                 <svg width="22" height="22" viewBox="0 0 22 22" fill="none"
                   xmlns="http://www.w3.org/2000/svg">
                   <path d="M2.44653 4L10.4465 11L2.44653 19" stroke="#FCC997" stroke-width="3.77953"
@@ -191,8 +212,8 @@
             </li>
 
             <li>
-              <button class="group py-2 pr-4 disabled:cursor-not-allowed" type="button"
-                x-on:click="getSubmission(pages.length - 1)" :disabled="pageNumber == (pages.length - 1)">
+              <button class="group py-1 pr-3 disabled:cursor-not-allowed" type="button"
+                x-on:click="viewPage(pages.length - 1)" :disabled="pageNumber == (pages.length - 1)">
                 <svg width="22" height="22" viewBox="0 0 22 22" fill="none"
                   xmlns="http://www.w3.org/2000/svg">
                   <path d="M11.4465 4L19.4465 11L11.4465 19" stroke="#FF5927" stroke-width="3.77953"
