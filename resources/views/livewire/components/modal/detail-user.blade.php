14f9460b96244682d3e534870f6787705be93cb4
euxzy <rijal.m12l@gmail.com>
2023-01-19 12:00:20 +0700

    [ + ] add pagination on list submission detail user

--- resources/views/livewire/components/modal/detail-user.blade.php
+++ resources/views/livewire/components/modal/detail-user.blade.php
@@ -2,7 +2,14 @@
 
   <script>
     Alpine.data('submission', () => ({
+      showMessage: 'Please wait...',
       submissionData: [],
+      pageNumber: 0,
+      size: 5,
+      total: '',
+      listSubmissions: [],
+      startAt: 0,
+      pages: [],
       getSubmission(id) {
         const reqTime = Date.now()
         const path = `/api/loan/list`
@@ -22,6 +29,23 @@
         }).then(async res => {
           this.submissionData = await res.json()
           // console.log(this.submissionData)
+
+          if (this.submissionData.data) {
+            const start = this.pageNumber * this.size
+            const end = start + this.size
+            this.total = this.submissionData.data.loans.length
+
+            this.listSubmissions = this.submissionData.data.loans.slice(start, end)
+            // console.log(this.listSubmissions)
+
+            this.pages = Array.from({
+              length: Math.ceil(this.total / this.size)
+            }, (val, i) => i)
+            // console.log(this.pages)
+          }
+
+
+          this.showMessage = 'No data submission found!'
         }).catch(err => {
           Swal.fire({
             icon: 'error',
@@ -29,6 +53,23 @@
             text: 'Internal Server Error! Please Try Again Later.',
           })
         })
+      },
+      async viewPage(index) {
+        // console.log(this.submissionData)
+        this.listSubmissions = []
+        this.pageNumber = index
+
+        const start = this.pageNumber * this.size
+        const end = start + this.size
+        // console.log(start, end)
+
+        this.startAt = start
+        this.listSubmissions = await this.submissionData.data.loans.slice(start, end)
+        // console.log(this.listSubmissions)
+
+        if (this.listSubmissions.length == 0) {
+          this.showMessage = 'No data Submissions found!'
+        }
       }
     }))
   </script>
@@ -98,10 +139,16 @@
         <li class="w-24 lg:w-56">Status</li>
       </ul>
 
+      <template x-if="listSubmissions.length == 0">
+        <div class="my-10 text-center text-2xl font-bold text-navy">
+          <h1 x-text="showMessage"></h1>
+        </div>
+      </template>
+
       <template x-if="submissionData.data">
-        <template x-for="(loan, i) of submissionData.data.loans">
+        <template x-for="(loan, i) of listSubmissions">
           <ul class="flex items-center gap-3 px-3 py-4 font-medium text-navy">
-            <li class="w-10 text-center" x-text="i + 1"></li>
+            <li class="w-10 text-center" x-text="startAt + i + 1"></li>
             <li class="flex w-32 flex-col lg:w-64">
               <span x-text="(new Date(loan.created_at)).toDateString()"></span>
               <span x-text="(new Date(loan.created_at)).toLocaleTimeString()"></span>
@@ -113,6 +160,83 @@
           </ul>
         </template>
       </template>
+
+      <template x-if="submissionData.data">
+        {{-- Start pagination --}}
+        <div class="flex justify-end py-8 px-10">
+          <ul class="flex w-max items-center rounded-md font-semibold text-orange-1 outline outline-2 outline-orange-1">
+            <li>
+              <button class="group py-1 pl-3 disabled:cursor-not-allowed" type="button" x-on:click="viewPage(0)"
+                :disabled="pageNumber == 0">
+                <svg width="22" height="22" viewBox="0 0 22 22" fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M11 4L3 11L11 19" stroke="#FF5927" stroke-width="3.77953" stroke-linejoin="round"
+                    class="group-hover:animate-arrowColor1" />
+                  <path d="M20 4L12 11L20 19" stroke="#FCC997" stroke-width="3.77953" stroke-linejoin="round"
+                    class="group-hover:animate-arrowColor2" />
+                </svg>
+              </button>
+            </li>
+
+            <li>
+              <button class="group py-1 pr-4 disabled:cursor-not-allowed" type="button"
+                x-on:click="viewPage(pageNumber - 1)" :disabled="pageNumber == 0">
+                <svg width="22" height="22" viewBox="0 0 22 22" fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M20 4L12 11L20 19" stroke="#FCC997" stroke-width="3.77953" stroke-linejoin="round"
+                    class="transition-all duration-200 group-hover:stroke-orange-2/60" />
+                </svg>
+              </button>
+            </li>
+
+            <li class="-mr-[.25px]">
+              <button type="button" x-on:click="viewPage(0)"
+                class="flex h-9 w-6 cursor-pointer items-center justify-center outline outline-1 outline-orange-1"
+                :class="pageNumber == 0 ? 'bg-orange-1 text-white' : ''">1</button>
+            </li>
+
+            <template x-for="num in pages">
+              <li class="-mr-[.25px]" :hidden="pageNumber != num || pageNumber == 0 || pageNumber == pages.length - 1">
+                <button type="button" x-on:click="viewPage(num)"
+                  class="flex h-9 w-6 cursor-pointer items-center justify-center outline outline-1 outline-orange-1"
+                  x-text="num + 1" :class="pageNumber == num ? 'bg-orange-1 text-white' : ''"></button>
+              </li>
+            </template>
+
+            <li class="-mr-[.25px]" :hidden="pages.length - 1 == 0">
+              <button type="button" x-on:click="viewPage(pages.length - 1)"
+                class="flex h-9 w-6 cursor-pointer items-center justify-center outline outline-1 outline-orange-1"
+                x-text="pages.length" :class="pageNumber == pages.length - 1 ? 'bg-orange-1 text-white' : ''"></button>
+            </li>
+
+            <li>
+              <button class="group py-1 pl-4 disabled:cursor-not-allowed" type="button"
+                x-on:click="viewPage(pageNumber + 1)" :disabled="pageNumber == (pages.length - 1)">
+                <svg width="22" height="22" viewBox="0 0 22 22" fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M2.44653 4L10.4465 11L2.44653 19" stroke="#FCC997" stroke-width="3.77953"
+                    stroke-linejoin="round" class="transition-all duration-200 group-hover:stroke-orange-2/60" />
+                </svg>
+              </button>
+            </li>
+
+            <li>
+              <button class="group py-1 pr-3 disabled:cursor-not-allowed" type="button"
+                x-on:click="viewPage(pages.length - 1)" :disabled="pageNumber == (pages.length - 1)">
+                <svg width="22" height="22" viewBox="0 0 22 22" fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M11.4465 4L19.4465 11L11.4465 19" stroke="#FF5927" stroke-width="3.77953"
+                    stroke-linejoin="round" class="group-hover:animate-arrowColor1" />
+                  <path d="M2.44653 4L10.4465 11L2.44653 19" stroke="#FCC997" stroke-width="3.77953"
+                    stroke-linejoin="round" class="group-hover:animate-arrowColor2" />
+                </svg>
+              </button>
+            </li>
+          </ul>
+        </div>
+        {{-- End Pagination --}}
+      </template>
+
     </div>
   </div>
 
